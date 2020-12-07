64ed1cb8e1f5d6ee2d1459d3fb93a5c501d26eac
Rejah Anuvar <rejahanwer@gmail.com>
2020-12-07 17:49:00 +0530

    Initial commit

---
+++ memorize/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by Rejah Anuvar on 03/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        return HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding(8)
+        .foregroundColor(.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        return ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Rejah Anuvar on 03/12/20.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
